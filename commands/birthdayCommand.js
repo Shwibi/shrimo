677cab81a3d77fea60d667ead78e4ffe7df9f9e1
Shwi <63779128+Shwibi@users.noreply.github.com>
2020-08-22 08:33:57 +0530

    BIRTHDAYYY!!!

--- commands/birthdayCommand.js
+++ commands/birthdayCommand.js
@@ -1,18 +1,34 @@
 module.exports = {
     name: 'birthday',
     execute(message, client){
+        message.delete();
+
+        if(message.member.roles.cache.has('746214235867971675') || message.member.roles.cache.has('746563022733967472')) return;
         const verify = message.guild.channels.cache.get(ch => ch.name === 'verify') || client.channels.cache.get('746217071620128879');
 
-        // if(message.channel.id != verify.id) return;
+        if(message.channel.id != verify.id) return;
 
         const birthday = message.content.split(" ")[1].split("/");
         if(!birthday) return;
-        if(birthday[0] > 12) return;
-        if(birthday[1] > 31) return;
-        if(birthday[2] > 99) return;
-
-        if(birthday[2] > 7) {
-            message.channel.send("no");
+        const d = new Date();
+        
+        const yearb = birthday[2];
+        const yearc = d.getFullYear();
+        const age = yearc - yearb;
+        if(age < 13) {
+            try {
+                message.member.roles.add('746563022733967472');
+            } catch (err) {
+                message.channel.send("Not enough permissions/roles. Error: ```" + err + "```");
+            }
+        } else {
+            try {
+                message.member.roles.add('746214235867971675');
+            } catch (err) {
+                message.channel.send("Not enough permissions/roles. Error: ```" + err + "```");
+            }
+            
+            
         }
         
     }
