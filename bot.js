22881e2751188d85316e65e419fd642ab3f49f21
Shwi <63779128+Shwibi@users.noreply.github.com>
2020-08-25 07:56:46 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,3 +30,11 @@ fs.readdir('./events/', (err, files) => {
         client.on(eventName, event.bind(null, client));
     })
 })
+
+var ipAddr = req.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = req.connection.remoteAddress;
+  }
