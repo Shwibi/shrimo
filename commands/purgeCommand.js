8ca4d30059a35889e0cbaf4bd2b0d38c707fb242
Shwi <63779128+Shwibi@users.noreply.github.com>
2020-08-26 19:01:44 +0530

    Purge command

---
+++ commands/purgeCommand.js
@@ -0,0 +1,24 @@
+const { execute } = require("./suggestCommand");
+const { GuildMember } = require("discord.js");
+
+module.exports = {
+    name: 'purge',
+    help: '<prefix>purge <number> \nDelete a specific number of messages',
+    async execute(message, client) {
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return;
+        const args = message.content.split(" ");
+        if(!args[1]) return message.channel.send(" :x: | Please mention amount of messages to purge.");
+        if(!isNaN(args[1]) && args[1] <= 100 && args[1] > 2) {
+            message.channel.messages.fetch().then(
+                message.channel.bulkDelete(args[1] + 1).then(
+                    message.channel.send(" :white_check_mark: | Successfully deleted " + args[1] + " messages.").then(
+                        m => m.delete({ timeout: 5000 })
+                    )
+                )
+            )
+        } else {
+            message.channel.send(" :x: | Please mention a number between 2-100 to purge! Note that I can't delete messages which were made before 14 days due to discord limitations on deleting messages.");
+
+        }
+    }
+}
