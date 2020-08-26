2e342627c81327510de7d288a21ab4a7a54a36c0
Shwi <63779128+Shwibi@users.noreply.github.com>
2020-08-26 11:33:08 +0530

    Update helpCommand.js

--- commands/helpCommand.js
+++ commands/helpCommand.js
@@ -31,7 +31,7 @@ module.exports = {
                     },
                     {
                         name: ":brain: Core",
-                        value: 'birthday, help, settings, \nslow, suggest, invite, suggestion',
+                        value: 'birthday, help, settings, \nslow, suggest, invite, suggestion, \nticket',
                         inline: true
                     },
                     {
