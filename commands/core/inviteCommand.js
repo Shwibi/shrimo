109a539b4ef07ff28deb5492e9fae6908006edfe
Shwi <63779128+Shwibi@users.noreply.github.com>
2020-08-30 12:16:19 +0530

    Help

--- commands/core/inviteCommand.js
+++ commands/core/inviteCommand.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'invite',
     help: '<prefix>invite \nGets the invite link for the bot! Thanks!',
     async execute(message, client) {
+        const { emoji } = require('../../config/config.json');
         const embed = {
             author: {
                 name: client.user.username,
@@ -12,13 +13,18 @@ module.exports = {
             color: 0xfcba03,
             fields: [
                 {
-                    name: "Invite The Bot",
-                    value: '[Invite](https://discord.com/oauth2/authorize?client_id=746202084147331092&scope=bot&permissions=8)',
+                    name: `<:Wumpi:748836768543146105> Invite Link!`,
+                    value: '[»» Invite ««](http://discord.com/oauth2/authorize?client_id=746202084147331092&scope=bot&permissions=8)',
                     inline: true
                 },
                 {
-                    name: "Support Server",
-                    value: '[Join](https://discord.gg/FtJ3QGc)',
+                    name: `${emoji.diamond} Support server`,
+                    value: '[»» Join ««](https://discord.gg/FtJ3QGc)',
+                    inline: true
+                },
+                {
+                    name: `${emoji.dev} Dev Server`,
+                    value: `[»» Join ««](https://discord.gg/fDuhPKq)`,
                     inline: true
                 }
             ]
